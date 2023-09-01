9598ab90e754a143df8eb388c0f814c209dc6760
RUFAT RESUL <rufatresul997@gmail.com>
2023-09-01 22:25:57 +0400

    final

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
-    'crispy_forms',
     "django_bootstrap5",
     'shop',
     'customer',
